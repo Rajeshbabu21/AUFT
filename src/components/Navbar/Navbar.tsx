5bf908d7fbc73cd549d7dbfa0b352e589a5968f4
rajeshgit2004@gmail.com <rajeshgit2004@gmail.com>
2025-07-03 11:03:16 +0530

    Added tsx fils

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
   return (
     <div>
       <nav className='bg-white border-gray-200 dark:bg-gray-900'>
-        <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
           <a
             href='https://flowbite.com/'
             className='flex items-center space-x-3 rtl:space-x-reverse'
@@ -27,7 +27,7 @@ const Navbar: React.FC = () => {
           >
             <span className='sr-only'>Open main menu</span>
             <svg
-              class='w-5 h-5'
+              className='w-5 h-5'
               aria-hidden='true'
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -35,9 +35,9 @@ const Navbar: React.FC = () => {
             >
               <path
                 stroke='currentColor'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M1 1h15M1 7h15M1 13h15'
               />
             </svg>
@@ -46,7 +46,7 @@ const Navbar: React.FC = () => {
             <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
                 <a
-                 href='/dashboard'
+                  href='/dashboard'
                   className='block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
                   aria-current='page'
                 >
