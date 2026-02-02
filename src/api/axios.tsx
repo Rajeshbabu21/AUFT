653ad888e7aa78e3ba2818c9c4441c7c5ec12ae6
Rajeshbabu21 <rajeshgit2004@gmail.com>
2026-02-03 03:01:34 +0530

    fix:url

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    
-    //  baseURL: import.meta.env.VITE_BACKEND_URL,
+
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     // baseURL: 'https://auft.onrender.com/',
-    baseURL: 'http://127.0.0.1:8000',   
+    // baseURL: 'http://127.0.0.1:8000',   
 
 })
 // console.log("AXIOS BASE URL:", import.meta.env.VITE_BACKEND_URL);
@@ -14,7 +14,7 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('access_token');
-    if (token) {  
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
